9e61c574743fe336edad273a111074d3262e8a59
sasha <sander_@inbox.ru>
2025-01-31 13:53:19 +0300

    added templates

---
+++ app/Http/Controllers/Auth/RegisteredUserControllerCustom.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserControllerCustom extends RegisteredUserController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'min:1'],
+            'email' => ['required', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+        flash('');
+        return redirect(route('dashboard', absolute: false));
+    }
+}
